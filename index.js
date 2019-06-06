ed07284a3538c47337a3993180e7925562339259
Jose Marin <josemarinemail@gmail.com>
2019-06-06 17:56:06 -0400

    Moved server logic to own module, and created a workers module.

--- index.js
+++ index.js
@@ -1,121 +1,13 @@
-// Built in node core requires
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const fs = require('fs');
-const StringDecoder = require('string_decoder').StringDecoder;
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-// Local requires
-const config = require('./lib/config');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+const app = {};
 
-// tmp testing
-helpers.sendTwilioSms('15005550007', 'Hello world!', err => {
-  console.error({ 'ERROR': err });
-});
-
-// HTTP server
-const httpServer = http.createServer((req, res) => {
-  unifiedServer(req, res);
-});
-
-httpServer.listen(config.httpPort, () => {
-  console.log(`server listening for requests on port ${config.httpPort}
-in the ${config.envName} environment`);
-});
-
-// HTTPS server
-const httpsServerOps = {
-  'key': fs.readFileSync('./https/key.pem'),
-  'cert': fs.readFileSync('./https/cert.pem')
+app.init = () => {
+  server.init();
+  workers.init();
 };
 
-const httpsServer = https.createServer(httpsServerOps, (req, res) => {
-  unifiedServer(req, res);
-});
+app.init();
 
-httpsServer.listen(config.httpsPort, () => {
-  console.log(`server listening for requests on port ${config.httpsPort}
-in the ${config.envName} environment`);
-});
-
-const unifiedServer = (req, res) => {
-  // Get the URL and parse it
-  const parsedUrl = url.parse(req.url, true);
-
-  // Get the path
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+s/g, '');
-
-  // Get the query string as an object
-  const queryStringObj = parsedUrl.query;
-
-  // Get the HTTP method
-  const method = req.method.toLowerCase();
-
-  // Get the headers as an object
-  const headers = req.headers;
-
-  // Get the payload if there is any
-  const decoder = new StringDecoder('utf-8');
-  let buffer = '';
-
-  req.on('data', data => {
-    buffer += decoder.write(data);
-  });
-
-  req.on('end', () => {
-    buffer += decoder.end();
-
-    // Choose the handler the request should go to
-    const chosenHandler = typeof (router[trimmedPath]) !== 'undefined'
-      ? router[trimmedPath]
-      : handlers.notFound;
-
-    // Construct the data object to send to the handler
-    const data = {
-      trimmedPath,
-      queryStringObj,
-      method,
-      headers,
-      'payload': helpers.parseJsonToObj(buffer)
-    };
-
-    // Route the request to the handler specified in the router
-    chosenHandler(data, (err, status, payload) => {
-      if (err) {
-        console.error(err);
-      }
-      status = typeof (status) === 'number' ? status : 200;
-
-      payload = typeof (payload) === 'object' ? payload : {};
-
-      // Convert the payload to a string
-      const payloadString = JSON.stringify(payload);
-
-      // Send the response
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(status);
-      res.end(payloadString);
-
-      // Log the request
-      console.log({
-        'status': status,
-        'request_path': trimmedPath,
-        'method': method,
-        'query_params': queryStringObj,
-        'headers': headers,
-        'payload': payloadString
-      });
-    });
-  });
-};
-
-// Request router
-const router = {
-  'ping': handlers.ping,
-  'users': handlers.users,
-  'tokens': handlers.tokens,
-  'checks': handlers.checks
-};
+module.exports = app;
